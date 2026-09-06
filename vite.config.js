ec48b5ef5f2f9161ff5250e0b4bece178bff0143
natianatmeladze <natia.natmeladzedoit04@geolab.edu.ge>
2026-09-06 15:53:59 +0400

    finished product

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/homework-react2/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
-})
+});
